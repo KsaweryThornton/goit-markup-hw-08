6b28aad5a1c6ef929778a9d7dc020c6069bc4b19
KsaweryThornton <ksawery.feliks.thornton@gmail.com>
2022-12-06 14:42:47 +0100

    Responsive web design.

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const menuOverlay = document.querySelector(".mobile-menu");
+const closeMenuButton = document.querySelector(".toggle-menu");
+const openMenuBtn = document.querySelector(".header__logo");
+
+openMenuBtn.addEventListener("click", (e) => {
+  menuOverlay.classList.remove("display-none");
+});
+
+closeMenuButton.addEventListener("click", (e) => {
+  menuOverlay.classList.add("display-none");
+});
